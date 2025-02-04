a6bcb36f4c9c17b98f21bd36a389316571668afc
Cent0Quatre <133564178+Cent0Quatre@users.noreply.github.com>
2025-02-04 21:56:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import os
 
 idx = np.random.randint(0, 10000)
 
-if not all(os.path.exists(f) for f in ["weights/W1.npy", "weights/b1.npy", "weights/W2.npy", "weights/b2.npy"]):
+if not all(os.path.exists(f) for f in ["weights/W1.npy", "weights/b1.npy", "weights/W2.npy", "weights/b2.npy", "weights/W3.npy", "weights/b3.npy"]):
     gradient_descent(X_train, Y_train, 0.1, 500)
 
 W1 = np.load("./weights/W1.npy")
 b1 = np.load("./weights/b1.npy")
 W2 = np.load("./weights/W2.npy")
 b2 = np.load("./weights/b2.npy")
+W3 = np.load("./weights/W3.npy")
+b3 = np.load("./weights/b3.npy")
 
-test_prediction(idx, W1, b1, W2, b2)
+test_prediction(idx, W1, b1, W2, b2, W3, b3)
+print(get_accuracy(make_predictions(X_train, W1, b1, W2, b2, W3, b3), Y_train))
+display_layer_outputs(X_train, W1, b1, W2, b2, W3, b3)
