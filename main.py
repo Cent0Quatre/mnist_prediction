52334059397416d83e07947f60ae8f8b58be55be
Cent0Quatre <133564178+Cent0Quatre@users.noreply.github.com>
2025-02-12 20:46:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from learn import *
 import os
 
-idx = np.random.randint(0, 10000)
-
 if not all(os.path.exists(f) for f in ["weights/W1.npy", "weights/b1.npy", "weights/W2.npy", "weights/b2.npy", "weights/W3.npy", "weights/b3.npy"]):
     gradient_descent(X_train, Y_train, 0.1, 500)
 
@@ -13,6 +11,10 @@ b2 = np.load("./weights/b2.npy")
 W3 = np.load("./weights/W3.npy")
 b3 = np.load("./weights/b3.npy")
 
-test_prediction(idx, W1, b1, W2, b2, W3, b3)
-print(get_accuracy(make_predictions(X_train, W1, b1, W2, b2, W3, b3), Y_train))
-display_layer_outputs(X_train, W1, b1, W2, b2, W3, b3)
+# print(get_accuracy(make_predictions(X_train, W1, b1, W2, b2, W3, b3), Y_train))
+
+print(load_and_preprocess_image("./dessin-28x28.png"))
+test_img_prediction("./dessin-28x28.png", W1, b1, W2, b2, W3, b3)
+
+#test_prediction(343, W1, b1, W2, b2, W3, b3)
+# LE PROBLEME VIENT DE LA CONVERSION DE L4IMAGE NUL A CIER
